5e413c632c67faa7e0be14d7af25bf22c6f4d8ea
kalasoo <ym.kalasoo@gmail.com>
2014-02-13 13:54:23 +0000

    matrix algebra

--- algebra.ml
+++ algebra.ml
@@ -68,6 +68,60 @@ module MartelliSemiring (Ord : Set.OrderedType) = struct
 
 end
 
+module MatrixMartelliSemiring (Ord : Set.OrderedType) = struct
+  
+  module MS = MartelliSemiring(Ord)
+
+  let plus n a b = 
+    let m = Array.make_matrix n n MS.zero in
+    for i = 0 to n - 1 do
+      for j = 0 to n - 1 do
+        m.(i).(j) <- MS.plus a.(i).(j) b.(i).(j)
+      done;
+    done;
+    m
+
+  let inner_times n a_row b_col =
+    let result = ref (MS.zero) in
+    Array.iteri (fun i a_i -> result := MS.plus !result (MS.times a_i b_col.(i)) ) a_row;
+    !result
+
+  (* get jth column of m *)
+  let column n j m =
+    if j < n then 
+      let col = Array.make n MS.zero in
+      for i = 0 to n - 1 do 
+        col.(i) <- m.(i).(j)
+      done;
+      col
+    else 
+      raise (Failure "Column out of bounds.")
+
+  let times n a b =
+    let m = Array.make_matrix n n MS.zero in
+    for i = 0 to n - 1 do
+      let a_row = a.(i) in
+      for j = 0 to n - 1 do
+        let b_col = column n j b in
+        m.(i).(j) <- inner_times n a_row b_col
+      done;
+    done;
+    m
+
+  let zero n =
+    Array.make_matrix n n MS.zero
+
+  let one n = 
+    let m = Array.make_matrix n n MS.zero in
+    for i = 0 to n - 1 do
+      for j = 0 to n - 1 do
+        if i = j then m.(i).(j) <- MS.one
+      done;
+    done;
+    m
+
+end
+
 let l1 = ["a"]
 let l2 = ["b"; "c";]
 let l3 = ["d"; "e"; "f"]
