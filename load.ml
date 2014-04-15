913d980c0dd0a9f6b99aa43e421f71fd4e1b7c24
kalasoo <ym.kalasoo@gmail.com>
2014-04-16 00:42:14 +0100

    add output

--- load.ml
+++ load.ml
@@ -10,46 +10,48 @@ let print_graph g =
   Digraph.print_gml fmt g
 
 let load_resources file =
-  let ic = open_in file in 
-  let l = ref [] in
   try
-    while true do
-      let line = input_line ic in
-      l := !l @ [List.map int_of_string (Str.split (Str.regexp " ") line)]
-    done;
-    assert false
-  with End_of_file -> 
-    close_in ic;
-    Array.of_list !l
+    let ic = open_in file in
+    let l = ref [] in
+    try
+      while true do
+        let line = input_line ic in
+        l := !l @ [List.map int_of_string (Str.split (Str.regexp " ") line)]
+      done;
+      assert false
+    with End_of_file ->
+      close_in_noerr ic;
+      Array.of_list !l
+  with e ->
+    print_endline ("Resource file [" ^ file ^ "] is not found");
+    [||]
 
 (* Definitions *)
-let () = print_endline "load graph and resources:"
-let graph = ref "graphs/basic.gml"
-let use_resource = ref false
-let rfile = ref "graphs/basic_resource"
-let output_graphml = ref "graphs/output.xml"
+let () = print_endline "load graph"
+let input_gml_file = ref "graphs/basic.gml"
+let input_resource_file = ref ""
+let output_graphml_file = ref ""
 let () =
   Arg.parse
-      ["-g", Arg.Set_string graph,
+      ["-g", Arg.Set_string input_gml_file,
        " <String>  path to gml file";
-       "-r", Arg.Set use_resource,
-       " Use resource file";
-       "-f", Arg.Set_string rfile,
+       "-r", Arg.Set_string input_resource_file,
        " <String>  path to resource file";
-       "-o", Arg.Set_string output_graphml,
+       "-o", Arg.Set_string output_graphml_file,
        " <String>  path to output graphml file";
       ]
       (fun _ -> ())
       "usage: ./load <options>"
 
-let graph = Digraph.parse_gml_file !graph
-let resource = load_resources !rfile
+let graph = Digraph.parse_gml_file !input_gml_file
+let use_resource = (!input_resource_file <> "")
+let to_file = (!output_graphml_file <> "")
+let resource = if use_resource then load_resources !input_resource_file else [||]
 
 let () =
-  if !use_resource then
-    let combine_list s num = 
-      s ^ " " ^ string_of_int(num)
-    in
+  if use_resource then
+    let combine_list s num = s ^ " " ^ string_of_int(num) in
+    print_endline "\nload resources:";
     Array.iteri (fun i l -> print_endline (string_of_int(i) ^ ": " ^ (List.fold_left combine_list "" l)) ) resource
 
 let n = Digraph.nb_vertex graph
@@ -57,7 +59,7 @@ let m = Digraph.nb_edges graph
 module MMS = MatrixMartelliSemiring(Int)
 
 (* Semiring *)
-let () = print_endline "\ngenerate mms:\n"
+let () = print_endline "\ngenerate mms:"
 let generate_mms graph =
   let zo = MMS.MS.zero 
   and l = ref [] 
@@ -68,7 +70,7 @@ let generate_mms graph =
     Digraph.iter_vertex (fun target -> begin
       try 
         let e = Digraph.find_edge graph source target in
-        if !use_resource then
+        if use_resource then
           l := !l @ [ MMS.MS.init_from_list (list_of_list resource.(Digraph.E.label e)) ]
         else
           l := !l @ [ MMS.MS.init_from_list [[Digraph.E.label e]] ]
@@ -94,6 +96,13 @@ module GraphmlPrinter = Graphml.Print(Digraph)(struct
 end);;
 
 let() =
-  print_endline "\noutput graphml:\n";
-  GraphmlPrinter.print Format.std_formatter graph
+  if to_file then
+    let oc = open_out !output_graphml_file in
+    let fmt = Format.formatter_of_out_channel oc in
+    print_endline ("\noutput graphml to file [" ^ !output_graphml_file ^ "]\n");
+    GraphmlPrinter.print fmt graph;
+    close_out oc
+  else
+    print_endline ("\noutput graphml:\n");
+    GraphmlPrinter.print Format.std_formatter graph
 
