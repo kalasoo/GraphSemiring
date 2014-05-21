ce39931e038bb015a3e1792e39c2a06274f5e445
kalasoo <ym.kalasoo@gmail.com>
2014-05-21 14:56:43 +0100

    remove old compile

--- compile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-ocamlc -o algebra algebra.ml
-ocamlfind ocamlc -o digraph  -linkpkg -package ocamlgraph digraph.ml
-ocamlfind ocamlc -o load -linkpkg -package ocamlgraph algebra.cmo digraph.cmo str.cma load.ml
