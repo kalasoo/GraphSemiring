23f04ec51d521713eb3cfbc874ca123d4d25e1ea
kalasoo <ym.kalasoo@gmail.com>
2014-05-22 12:06:06 +0100

    merge to one file

--- load.ml
+++ load.ml
@@ -1,22 +1,23 @@
 open Core.Std
 open Graphloader
-open Semiringloader
+open Semiring
 
 let print_section s = print_endline ("\n>>> " ^ s)
 
-(* Definitions *)
-let () = print_section "Load graph"
+(* Args *)
 let input_gml_file = ref "graphs/basic.gml"
 let () =
   Arg.parse
       ["-g", Arg.Set_string input_gml_file,
-       " <String>  path to gml file";
+       " <String> path to gml file";
       ]
       (fun _ -> ())
-      "usage: ./load <options>"
+      "usage: ./load.byte <options>"
 
 let g = graph_of_gml !input_gml_file
 
+(* Load graph *)
+let () = print_section "Load graph"
 let () =
   print_endline "nodes:";
   G.iter_vertex (fun v -> 
@@ -29,6 +30,22 @@ let () =
     printf "\t[%d %d]: %s\n%!" (G.V.label s) (G.V.label t) (G.E.label e)
   ) g
 
-
+(* Make Martelli Semiring *)
 let () = print_section "Make Martelli Semiring"
-let m = create_martelli_semiring g
+let semiring_of_graph g zero create =
+  let n = G.nb_vertex g in
+  let m = zero n    in
+  G.iter_edges_e (fun e -> 
+    let s = G.V.label (G.E.src e) in
+    let t = G.V.label (G.E.dst e) in
+    let r = G.E.label e           in
+    m.(s).(t) <- (create r)
+  ) g;
+  m
+let m = semiring_of_graph g MMS.zero MS.create
+  
+(* Make Martelli Semiring *)
+let () = print_section "Solve Semiring"
+let () =
+  let solved = MMS.solve m in
+  MMS.print solved
--- semiring.ml
+++ semiring.ml
@@ -1,3 +1,5 @@
+open Core.Std
+
 (** Semiring *)
 module type S = sig
   
@@ -13,6 +15,10 @@ module type S = sig
   val plus   : t -> t -> t
 
   val times  : t -> t -> t
+
+  val create : string -> t
+
+  val to_string : t -> string
 
 end
 
@@ -67,7 +73,7 @@ module Make_Matrix_Semiring (Semiring : S) = struct
     !result
 
   let column n j matrix =
-    let col = Array.make n Semiring.zero in
+    let col = Array.create n Semiring.zero in
     for i = 0 to n - 1 do
       col.(i) <- matrix.(i).(j)
     done;
@@ -97,4 +103,199 @@ module Make_Matrix_Semiring (Semiring : S) = struct
     done;
     !_m
 
+  let print a =
+    let n = size a in
+    for i = 0 to n - 1 do
+      for j = 0 to n - 1 do
+        printf "\t%s;" (Semiring.to_string a.(i).(j))
+      done;
+      print_char '\n'
+    done;
+
 end
+
+(** Min Plus Semiring *)
+module MPS = struct
+
+  exception Negative_value
+  
+  type t = 
+  | Val of int
+  | Infinity
+
+  let zero  = Infinity
+
+  let one   = Val 0
+
+  let create s =
+    let i = Int.of_string s in
+    if i >= 0 then Val i
+    else raise Negative_value
+
+  let to_string a =
+    match a with
+    | Val i -> Int.to_string i
+    | Infinity -> "Infinity"
+
+  let reduce a = a
+
+  let plus a b =
+    match a with
+    | Infinity -> b
+    | Val a'   ->
+      match b with
+      | Infinity -> a
+      | Val b'   -> Val (min a' b')
+
+  let times a b =
+    match a, b with
+    | _, Infinity | Infinity, _ -> Infinity
+    | Val a', Val b'            -> Val (a' + b')
+
+end
+
+module MMPS = Make_Matrix_Semiring(MPS)
+
+(** Martelli Semiring *)
+module MS = struct
+  
+  module S = Set.Make(String)
+
+  include Set.Make(S)
+
+  let zero = add empty S.empty
+
+  let one = empty
+
+  let reduce a = 
+    let not_subset set = for_all a ~f:(fun a_set -> not (S.subset a_set set) || S.equal a_set set) in
+    filter a ~f:not_subset
+
+  let plus a b =
+    let fold_element s a_set = fold b ~init:s ~f:(fun s_iter b_set -> add s_iter (S.union a_set b_set)) in
+    reduce (fold a ~init:empty ~f:fold_element )
+
+  let times a b =
+    reduce (union a b)
+
+  let create s =
+    reduce (t_of_sexp (Sexp.of_string s))
+
+  let to_string a =
+    Sexp.to_string (sexp_of_t a)
+
+end
+
+module MMS = Make_Matrix_Semiring(MS)
+
+(** Boolean Semiring *)
+module BS = struct
+
+  module B = Core.Blang
+
+  type t = string B.t
+
+  let zero = B.false_
+
+  let one  = B.true_
+
+  let compare_elt = B.compare String.compare
+
+  let contains s1 s2 =
+    let re = Str.regexp_string s2
+    in
+      try ignore (Str.search_forward re s1 0); true
+      with Not_found -> false
+
+  let string_of_product product = 
+    let l = B.values product in
+    String.of_char_list (List.map l ~f:Char.of_string)
+
+  let compare_product a b =
+    let a_str = string_of_product a  in
+    let b_str = string_of_product b  in
+    let compare_leng = Int.compare (String.length a_str) (String.length b_str) in
+      if compare_leng = 0 then String.compare a_str b_str
+      else compare_leng
+
+  let rec expand f =
+    match f with
+    | B.Or  (g, h) -> B.or_ [expand g; expand h]
+    | B.And (g, h) -> 
+      let g_list = B.gather_disjuncts (expand g) in
+      let h_list = B.gather_disjuncts (expand h) in
+      let expanded_list = List.fold g_list ~init:[] ~f:(fun l g_component -> 
+          List.append l (
+            List.map h_list ~f:(fun h_component ->
+              B.and_ [g_component; h_component]
+            )
+          )
+        )
+      in
+      B.or_ expanded_list
+    | B.Not f' -> (
+      match f' with
+      | B.Not f''    -> expand f''
+      | B.Or  (g, h) -> expand (B.and_ [B.not_ g; B.not_ h])
+      | B.And (g, h) -> expand (B.or_  [B.not_ g; B.not_ h])
+      | _            -> expand (B.not_ f'))
+    | _ -> f
+
+  let rec reduce_expanded f =
+    match f with
+    | B.Or (_, _) as sums ->
+      let s_list        = List.map (B.gather_disjuncts sums) ~f:reduce_expanded in
+      let s_sorted_list = List.sort ~cmp:compare_product s_list in
+      let reduced_strings = ref [] in
+      let reduced_l       = ref [] in
+      List.iter s_sorted_list ~f:(fun p -> 
+        let p_str = string_of_product p in
+        if List.exists !reduced_strings ~f:(fun str -> contains p_str str) then ()
+        else (
+          reduced_strings := (p_str :: !reduced_strings);
+          reduced_l       := (p     :: !reduced_l)
+        )
+      );
+      B.or_ (List.rev !reduced_l)
+    | B.And (_, _) as products ->
+      let p_list        = B.gather_conjuncts products in
+      let p_sorted_list = List.sort ~cmp:compare_elt p_list in
+      let rec remove_duplicates l acc =
+        match l with
+        | []  -> List.rev acc
+        | [x] -> List.rev (x::acc)
+        | x1 :: x2 :: tl ->
+          if (compare_elt x1 x2) = 0
+          then remove_duplicates (x2 :: tl) acc
+          else remove_duplicates (x2 :: tl) (x1 :: acc)
+      in
+      B.and_ (remove_duplicates p_sorted_list [])
+    | B.Not f' -> (
+      match f' with
+      | B.Not f'' -> reduce_expanded f''
+      | _         -> B.not_ (reduce_expanded f'))
+    | _ -> f
+
+  let reduce f = 
+    reduce_expanded (expand f)
+
+  let plus a b = 
+    if a = b then a
+    else reduce (B.or_ [a ; b])
+
+  let times a b =
+    if a = b then a
+    else reduce (B.and_ [a ; b])
+
+  let create s = 
+    reduce (B.t_of_sexp String.t_of_sexp (Sexp.of_string s))
+
+  let to_string f =
+    Sexp.to_string (B.sexp_of_t String.sexp_of_t f)
+
+  let test s =
+    to_string (create s)
+
+end
+
+module MBS = Make_Matrix_Semiring(BS)
